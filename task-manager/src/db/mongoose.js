08857504e0bb8391798edc6fb005b4eed0c9fdd6
Saad Afzaal <saad.afzaal777@gmail.com>
2019-03-03 00:51:19 +0500

    starting make api

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+
+
+// const me = new User({
+//     name: '   Jason',
+//     password: 'Password123', 
+//     email: 'jason@gmail.com    ',
+//     age: 27
+// });
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error: ', error);
+// });
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// });
+
+// const task = new Task({
+//     description: 'Learn the Mongoose Library', 
+//     completed: false
+// });
+
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((error) => {
+//     console.log('Error: ', error);
+// });
