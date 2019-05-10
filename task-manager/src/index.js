601f0be95c1c44ab2c3d049626bd5baa7377e42c
Saad Afzaal <saad.afzaal777@gmail.com>
2019-05-11 04:39:38 +0500

    new work

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,14 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled');
+//     }else{
+//         next();
+//     }
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -16,17 +24,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const myFunction = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '1 seconds'});
-    console.log(token);
+// const myFunction = async () => {
+//     const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '1 seconds'});
+//     console.log(token);
 
-    const data = jwt.verify(token, 'thisismynewcourse');
-    console.log(data);
-}
+//     const data = jwt.verify(token, 'thisismynewcourse');
+//     console.log(data);
+// }
 
-myFunction();
+// myFunction();
 
 // const bcrypt = require('bcryptjs');
 
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,7 +40,13 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number');
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -56,6 +63,16 @@ userSchema.statics.findByCredentials = async (email, password) => {
     }
 
     return user; 
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString()}, 'thisismynewcourse');
+    
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+
+    return token;
 }
 
 userSchema.pre('save', async function(next) {
